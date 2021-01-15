950fc05c09af32b23edc4996f3013aa0fc4b4e10
aretterra <54510967+aretterra@users.noreply.github.com>
2021-01-15 21:53:05 +0300

    конструктор

--- sandbox/src/main/java/ru/stqa/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/sandbox/MyFirstProgram.java
@@ -3,24 +3,19 @@ package ru.stqa.sandbox;
 public class MyFirstProgram {
 public static void main(String[] args){
 hello("world!");
-Square s = new Square(10);
+Square s = new Square(6);
 
 
-    System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s) );
-Rectangle r = new Rectangle(5, 7);
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area() );
+Rectangle r = new Rectangle(9, 2);
 
-    System.out.println("Площадь прямоугольника со сторонами " + r.a +" и " + r.b + " = " + area(r) );
+    System.out.println("Площадь прямоугольника со сторонами " + r.a +" и " + r.b + " = " + r.area() );
 }
 public static void hello(String somebody){
        System.out.println("hello, " + somebody);
 }
-public static double area(Square s){
-    return s.l * s.l;
-
-}
-public static double area(Rectangle r){
-    return r.a * r.b;
 
 }
 
-}
+
+
