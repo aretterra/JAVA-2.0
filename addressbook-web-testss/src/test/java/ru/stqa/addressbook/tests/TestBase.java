047757a07fdba24b8bd48ca6f50d8e1e30ed57ae
aretterra <54510967+aretterra@users.noreply.github.com>
2021-01-27 23:16:30 +0300

    31 equals

--- addressbook-web-testss/src/test/java/ru/stqa/addressbook/tests/TestBase.java
+++ addressbook-web-testss/src/test/java/ru/stqa/addressbook/tests/TestBase.java
@@ -9,7 +9,7 @@ import static org.testng.Assert.fail;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+    protected final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
 
     @BeforeClass(alwaysRun = true)
     public void setUp() throws Exception {
